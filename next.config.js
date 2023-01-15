00cbd40dfa69617fc45aa086043335d4fec5060e
Juan Santisteban <juandiegosantisteban115@gmail.com>
2023-01-15 12:54:19 -0500

    1v

--- next.config.js
+++ next.config.js
@@ -3,4 +3,38 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+
+
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+
+module.exports = {
+  webpack: (config) => {
+    config.plugins.push(
+      new WorkboxWebpackPlugin.GenerateSW({
+        swDest: 'service-worker.js',
+        clientsClaim: true,
+        skipWaiting: true,
+        runtimeCaching: [
+          {
+            urlPattern: /\.(jpg|mp4)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'my-pwa-cache',
+              expiration: {
+                maxEntries: 20,
+                maxAgeSeconds: 7 * 24 * 60 * 60,
+              },
+            },
+          },
+        ],
+        precacheManifestFilename: 'precache-manifest.[manifestHash].js',
+        importWorkboxFrom: 'local',
+        globDirectory: 'public',
+        globPatterns: ['assets/**/*'],
+      })
+    );
+    return config;
+  },
+};
+
 module.exports = nextConfig
